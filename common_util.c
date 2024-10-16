b178aae51593c58e9fd9c8f068411e6520d6cea2
Akhileshwari-45 <akhilagalagali5@gmail.com>
2024-10-16 16:12:24 +0530

    Update common_util.c

--- common_util.c
+++ common_util.c
@@ -53,3 +53,19 @@ battery_status check_battery_error(float value, float *range_arr)
     return warning;
 
 }
+
+void message_logger(char *str,float value, battery_status state)
+{
+    if(state == warning)
+    {
+        printf("warning: %s %f is breaching \n",str,value);
+    }
+    else if(state == error)
+    {
+        printf("Error: %s %f is out of range\n",str,value);
+    }
+    else
+    {
+        printf("No Error: %s %f is Normal\n",str,value);
+    }
+}
