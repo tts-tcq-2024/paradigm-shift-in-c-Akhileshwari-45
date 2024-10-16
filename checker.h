68a5c83934c368d2b6e5d3fbde0f9d21df211a31
Akhileshwari-45 <akhilagalagali5@gmail.com>
2024-10-16 16:11:46 +0530

    Update checker.h

--- checker.h
+++ checker.h
@@ -4,16 +4,7 @@
 #include<stdio.h>
 #include <stdbool.h>
 
-#define    min_tolerance  0
-#define    min            1
-#define    max_tolerance  2
-#define    max            3
-
-typedef enum
-{
-    error = 0,
-    warning,
-    normal
-}battery_status;
-
+battery_status battery_charge_rate(float value,float range);
+battery_status battery_soc_check(float value,float min_range,float max_range);
+battery_status battery_temperature_check(float value,float min_range,float max_range);
 #endif
