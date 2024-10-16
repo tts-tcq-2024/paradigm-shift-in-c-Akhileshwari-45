6342efb6ce36317eade11828ed6fa02a157d159a
Akhileshwari-45 <akhilagalagali5@gmail.com>
2024-10-16 16:19:42 +0530

    Update checker.h

--- checker.h
+++ checker.h
@@ -1,10 +1,7 @@
 #ifndef CHECKER_H
 #define CHECKER_H
-
 #include<stdio.h>
 #include <stdbool.h>
 
-battery_status battery_charge_rate(float value,float range);
-battery_status battery_soc_check(float value,float min_range,float max_range);
-battery_status battery_temperature_check(float value,float min_range,float max_range);
+int batteryIsOk(float temperature, float soc, float chargeRate);
 #endif
