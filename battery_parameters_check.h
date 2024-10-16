aa9522536fdbca7d0facf1449295486bc6db839c
Akhileshwari-45 <akhilagalagali5@gmail.com>
2024-10-16 16:34:37 +0530

    Update battery_parameters_check.h

--- battery_parameters_check.h
+++ battery_parameters_check.h
@@ -1,5 +1,6 @@
 #ifndef BATTERY_PARAMETERS_CHECK_H
 #define BATTERY_PARAMETERS_CHECK_H
+#include "common_util.h"
 
 battery_status battery_temperature_check(float value,float min_range,float max_range);
 battery_status battery_soc_check(float value,float min_range,float max_range);
