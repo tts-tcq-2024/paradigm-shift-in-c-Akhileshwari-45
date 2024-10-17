b4e95d8f4055d06748d9273a8f7f6ab098be78fe
Akhileshwari-45 <akhilagalagali5@gmail.com>
2024-10-17 09:05:32 +0530

    Update battery_parameters_check.c

--- battery_parameters_check.c
+++ battery_parameters_check.c
@@ -38,7 +38,6 @@ battery_status battery_charge_rate(float value,float range)
 
 }
 
-//battrey soc check
 battery_status battery_soc_check(float value,float min_range,float max_range)
 {
 
@@ -58,7 +57,6 @@ battery_status battery_soc_check(float value,float min_range,float max_range)
      return normal;
 }
 
-//battrey temperature check
 battery_status battery_temperature_check(float value,float min_range,float max_range)
 {
 
